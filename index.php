81b700e6c8f72b95743af35c7ccc176644746df5
Aquinoaq <eaquino790@gmail.com>
2025-04-28 15:21:06 -0500

    .

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="buscar.php" method="post">
+            <input type="text" name="buscar" id="">
+            <input type="submit" value="Buscar">
+            <a href="nuevo">nuevo</a>
+        </form>
+    </div>
+
+    <div>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>NOMBRES</td>
+                <td>APELLIDOS</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
